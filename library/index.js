14dd18c217b0a700b4ff8d38aaafa0acc1847f18
cesarhenrq <cesaarhvb@gmail.com>
2023-09-16 21:33:19 -0300

    8.15 Database, part 3

--- library/index.js
+++ library/index.js
@@ -1,10 +1,9 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { GraphQLError } = require("graphql");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-
-const { v1: uuid } = require("uuid");
 
 const Book = require("./schemas/book.schema");
 const Author = require("./schemas/author.schema");
@@ -23,84 +22,6 @@ mongoose
   .catch((error) => {
     console.log("error connection to MongoDB:", error.message);
   });
-
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "The Demon ",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
 
 const typeDefs = `
   type Book {
@@ -169,16 +90,38 @@ const resolvers = {
 
   Mutation: {
     addBook: async (root, args) => {
-      const author = await Author.findOne({ name: args.author });
+      let author = await Author.findOne({ name: args.author });
 
       if (!author) {
-        const author = new Author({ name: args.author });
-        await author.save();
+        author = new Author({ name: args.author });
+
+        try {
+          await author.save();
+        } catch (error) {
+          throw new GraphQLError("Saving author failed", {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args.author,
+              error,
+            },
+          });
+        }
       }
 
       const book = new Book({ ...args, author: author._id });
       await book.populate("author");
-      await book.save();
+
+      try {
+        await book.save();
+      } catch (error) {
+        throw new GraphQLError("Saving book failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: error.errors.title.value,
+            error,
+          },
+        });
+      }
 
       return book;
     },
@@ -187,16 +130,31 @@ const resolvers = {
       const author = await Author.findOne({ name: args.name });
 
       if (!author) {
-        return null;
+        throw new GraphQLError("Author not found", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+          },
+        });
       }
 
-      const updatedAuthor = await Author.findByIdAndUpdate(
-        author._id,
-        { born: args.setBornTo },
-        { new: true }
-      );
+      try {
+        const updatedAuthor = await Author.findByIdAndUpdate(
+          author._id,
+          { born: args.setBornTo },
+          { new: true }
+        );
 
-      return updatedAuthor;
+        return updatedAuthor;
+      } catch (error) {
+        throw new GraphQLError("Updating author failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: error.errors.name.value,
+            error,
+          },
+        });
+      }
     },
   },
 };
